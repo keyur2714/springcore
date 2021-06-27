e46676abcbc449153e2932a3285d49d0cda2aa43
keyur2714 <keyurjava27@gmail.com>
2021-06-27 23:54:48 +0530

    Updated with remove and insert with pos

--- LinkedListDemo/src/com/webstack/driver/Driver.java
+++ LinkedListDemo/src/com/webstack/driver/Driver.java
@@ -11,6 +11,28 @@ public class Driver {
 		myLinkedList.insert(11);
 		myLinkedList.insert(22);
 		myLinkedList.insert(33);
+		myLinkedList.insert(44);
+		myLinkedList.insert(55);
+		
+		System.out.println("After Insert");
+		
+		myLinkedList.display();
+		
+		myLinkedList.insertWithPos(1,100);
+		
+		System.out.println("After Position Insert");
+		
+		myLinkedList.display();
+						
+		myLinkedList.remove(1);
+		
+		System.out.println("After Remove First Node");
+		
+		myLinkedList.display();
+		
+		myLinkedList.remove(2);
+		
+		System.out.println("After Remove Position 2 Node");
 		
 		myLinkedList.display();
 		
--- LinkedListDemo/src/com/webstack/util/MyLinkdedList.java
+++ LinkedListDemo/src/com/webstack/util/MyLinkdedList.java
@@ -20,10 +20,46 @@ public class MyLinkdedList {
 		}		
 	}
 	
+	public void remove(int pos) {
+		Node currentNode = firstNode;
+		Node prevNode = null;
+		int i=1;
+		if(pos == 1) {
+			firstNode = firstNode.next;
+		}else {
+			while(currentNode != null && i < pos) {				
+				prevNode = currentNode;
+				currentNode = currentNode.next;
+				i++;
+			}			
+			prevNode.next = currentNode.next;					
+		}		
+	}
+	
+	public void insertWithPos(int pos,int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		if(pos == 1) {
+			newNode.next = firstNode;
+			firstNode = newNode;
+		}else {
+			Node currentNode = firstNode;
+			Node prevNode = null;
+			int i=1;
+			while(currentNode != null && i < pos) {				
+				prevNode = currentNode;
+				currentNode = currentNode.next;
+				i++;
+			}
+			prevNode.next = newNode;
+			newNode.next = currentNode;
+		}
+	}
+	
 	public void display() {
 		Node currentNode = firstNode;
 		while(currentNode != null) {
-			System.out.println(currentNode.data);
+			System.out.println(currentNode.data + "==");
 			currentNode = currentNode.next;
 		}
 	}
